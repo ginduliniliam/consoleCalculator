3dcb12532af1d7f958883d1a9fd5a19a8580c60d
Gindulin Ilia <gindulin.gho@gmail.com>
2024-08-01 09:13:24 +0300

    Creating variables and addition functions

---
+++ consoleCalculator.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var digital_1, digital_2 float64
+	var sign string
+
+	fmt.Print("Введите первое число:")
+	fmt.Scanln(&digital_1)
+
+	fmt.Print("Введите второе число:")
+	fmt.Scanln(&digital_2)
+
+	fmt.Print("Введите знак для операции(+,-,/,*):")
+	fmt.Scanln(&sign)
+
+	if sign == "+" {
+		meaning := digital_1 + digital_2
+		fmt.Println("= ", meaning)
+	}
+}
